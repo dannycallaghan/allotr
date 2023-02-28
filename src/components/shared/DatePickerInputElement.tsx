2af923a9a74225f712bf9746d25e14f4997cfd56
Danny Callaghan <dannycallaghan@gmail.com>
2023-02-28 22:41:29 +0000

    Removed unused component that was breaking the build

--- src/components/shared/DatePickerInputElement.tsx
+++
@@ -1,8 +0,0 @@
-// import { forwardRef } from 'react';
-
-// // eslint-disable-next-line react/display-name
-// export const DatePickerInputElement = forwardRef(({ value, onClick }, ref) => (
-//   <button className="example-custom-input" onClick={onClick} ref={ref}>
-//     {value}
-//   </button>
-// ));
