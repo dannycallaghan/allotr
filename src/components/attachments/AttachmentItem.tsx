4a234a4dc12bc7d436cd1a2069bece7bf29781c6
Danny Callaghan <dannycallaghan@gmail.com>
2023-03-09 16:57:53 +0000

    Improved attachments UI

--- src/components/attachments/AttachmentItem.tsx
+++ src/components/attachments/AttachmentItem.tsx
@@ -14,28 +14,44 @@ const Attachments = (props: IProps) => {
   const { data, file, remove } = props;
 
   return (
-    <div className="mb-4 rounded-lg bg-white">
-      <div
-        style={{ backgroundImage: `url(${file.secure_url})` }}
-        className="h-20 max-h-20 rounded-t-lg bg-cover bg-center"
-      ></div>
-      <div className="p-4">
-        <p className="overflow-hidden text-ellipsis">
-          <a
-            href={file.secure_url}
-            className="text-primary underline"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {file.secure_url}
-          </a>
-        </p>
-        <p>Added {formatAsFriendlyDate(new Date(file.created_at), true)}</p>
-        <div className="flex justify-end">
-          <RemoveAttachmentButton data={data} remove={() => remove(file.id)} />
+    <>
+      <div className="mb-4 flex flex-col rounded-lg bg-white p-2 sm:flex-row">
+        <div className="flex justify-center">
+          <div className="h-20 w-20 overflow-hidden rounded-md bg-gray-100">
+            <a href={file.secure_url} target="_blank" rel="noreferrer">
+              {file.thumbnail_url ? (
+                <img
+                  src={file.thumbnail_url}
+                  className="w-full"
+                  alt={file.secure_url}
+                />
+              ) : (
+                <span className="block h-full w-full">&nbsp;</span>
+              )}
+            </a>
+          </div>
+        </div>
+        <div className="block flex-grow overflow-hidden text-ellipsis p-4">
+          <p>
+            <a
+              href={file.secure_url}
+              className="block overflow-hidden text-ellipsis text-primary underline"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {file.secure_url}
+            </a>
+          </p>
+          <p>Added {formatAsFriendlyDate(new Date(file.created_at), true)}</p>
+          <div className="flex justify-end">
+            <RemoveAttachmentButton
+              data={data}
+              remove={() => remove(file.id)}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
