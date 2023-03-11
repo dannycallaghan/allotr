ffc711fd49629488fa421a54a8e984467bd15cef
Danny Callaghan <dannycallaghan@gmail.com>
2023-03-11 19:23:55 +0000

    Added link to manifest

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,15 @@
-const VERSION = 4;
+const VERSION = 5;
 const PRECACHE = `allotr-precache-v${VERSION}`;
 const RUNTIME = 'allotr-runtime';
 
 // A list of local resources we always want to be cached.
-const PRECACHE_URLS = [];
+const PRECACHE_URLS = [
+  'index.html',
+  './', // Alias for index.html
+  'styles.css',
+  '../../styles/main.css',
+  'demo.js',
+];
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', (event) => {
@@ -35,4 +41,29 @@ self.addEventListener('activate', (event) => {
       })
       .then(() => self.clients.claim()),
   );
+});
+
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', (event) => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then((cache) => {
+          return fetch(event.request).then((response) => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      }),
+    );
+  }
 });
