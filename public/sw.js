ac32e3b6f2697c6831da2def65804f779778bc54
Danny Callaghan <dannycallaghan@gmail.com>
2023-03-17 10:13:16 +0000

    Changed to  service worker

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 6;
+const VERSION = 7;
 const PRECACHE = `allotr-precache-v${VERSION}`;
 const RUNTIME = 'allotr-runtime';
 
@@ -41,29 +41,4 @@ self.addEventListener('activate', (event) => {
       })
       .then(() => self.clients.claim()),
   );
-});
-
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', (event) => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      }),
-    );
-  }
 });
