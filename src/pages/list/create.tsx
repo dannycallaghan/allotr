2c6ca2ea7469fcf41c56c57f7bd40973199ce828
Danny Callaghan <dannycallaghan@gmail.com>
2024-04-18 19:42:42 +0100

    Fixing a bug

--- src/pages/list/create.tsx
+++ src/pages/list/create.tsx
@@ -181,24 +181,4 @@ const CreatePage: NextPage = () => {
   );
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export async function getServerSideProps(context: any) {
-  // const session = await getSession(context);
-  // if (!session) {
-  //   let callback = '';
-  //   if (context && context.resolvedUrl) {
-  //     callback = `callbackUrl=${context.resolvedUrl}`;
-  //   }
-  //   return {
-  //     redirect: {
-  //       destination: `/auth/signin?${callback}`,
-  //       permanent: false,
-  //     },
-  //   };
-  // }
-  // return {
-  //   props: { session },
-  // };
-}
-
 export default CreatePage;
